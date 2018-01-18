7c7da9661f381daeee2f87ea2be60045f8bd730c
Codadillo <leoconr@nuevaschool.org>
2018-01-18 12:34:10 -0800

    init

---
+++ lib/mtg_sdk_dart.dart
@@ -0,0 +1,8 @@
+/// Support for doing something awesome.
+///
+/// More dartdocs go here.
+library mtg_sdk_dart;
+
+export 'src/mtg_sdk_dart_base.dart';
+
+// TODO: Export any libraries intended for clients of this package.
