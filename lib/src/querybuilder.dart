a35bd100e966225554f3224b7af61e3d63d489cf
Codadillo <leoconr@nuevaschool.org>
2018-01-22 09:43:47 -0800

    Added part to gather data from all pages for where().

--- lib/src/querybuilder.dart
+++ lib/src/querybuilder.dart
@@ -8,22 +8,29 @@ final cards card = new cards();
 final sets set = new sets();
 
 abstract class Product {
-  Future find(final dynamic id) async {
+  Future<dynamic> find(final dynamic id) async {
     final Response response =
         await client.get("$endpoint/${this.runtimeType}/$id");
     final dynamic decodedResponse = JSON.decode(response.body);
-    if (decodedResponse.containsKey("error")) return decodedResponse;
-    // print(decodedResponse.values.first);
+    if (decodedResponse.containsKey("error")) return decodedResponse; 
     return decodedResponse.values.first;
   }
 
-  Future where(final Map<String, dynamic> attributes) async {
-    final Response response = await client
-        .get("$endpoint/${this.runtimeType}?${assembleAttributes(attributes)}");
-    final dynamic decodedResponse = JSON.decode(response.body);
+  Future<dynamic> where(final Map<String, dynamic> attributes) async {
+    final String url = "$endpoint/${this.runtimeType}?${assembleAttributes(attributes)}";
+    print(url);
+    dynamic decodedResponse = JSON.decode((await client.get(url)).body);
     if (decodedResponse.containsKey("error")) return decodedResponse;
-    // print(decodedResponse.values.first)
-    return decodedResponse.values.first;
+    decodedResponse = decodedResponse.values.first;
+    if (attributes.containsKey("page")) return decodedResponse;
+    int page = 0;
+    dynamic pageResponse = [-1];
+    while (!pageResponse.isEmpty) {
+      ++page;
+      pageResponse = JSON.decode((await client.get("$url&page=$page")).body).values.first;
+      decodedResponse.addAll(pageResponse);
+    }
+    return decodedResponse;
   }
 
   String assembleAttributes(Map<String, dynamic> attributes) {
@@ -45,7 +52,8 @@ abstract class Product {
 class cards extends Product {}
 class sets extends Product {}
 
-main() {
-  print(set.where({}));
+main() async  {
+  dynamic response = (await card.where({"name": '"Storm Crow"'}));
+  print(response.length);
   // cards.where({"no": "yes"});
 }
