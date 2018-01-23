4b0c1ffbd99cd6d94038ede0e4decf47f713e3de
Codadillo <leoconr@nuevaschool.org>
2018-01-22 17:51:32 -0800

    Made set unit tests and all passed

---
+++ test-src/querybuilder_set_test.dart
@@ -0,0 +1,24 @@
+import 'package:test/test.dart';
+import '../lib/src/querybuilder.dart';
+
+void main() {
+  test("set.find() should return the set of the given id.", () async {
+    dynamic mirage = await set.find("MIR");
+    expect(mirage["name"], equals("Mirage"));
+    expect(mirage["border"], equals("black"));
+  });
+
+  test("set.where() should return sets of the given properties.", () async {
+    dynamic where_general_test = await set.where({"type": "un"});
+    expect(where_general_test.first["name"], equals("Unstable"));
+    expect(where_general_test.first["border"], equals("silver"));
+    expect(where_general_test.length, equals(3));
+  });
+
+  test("set.where() should be able to control the page number and size", () async {
+    dynamic where_general_test = await set.where({"page": "4", "pageSize": "2"});
+    expect(where_general_test.first["name"], equals("Super Series"));
+    expect(where_general_test.first["border"], equals("black"));
+    expect(where_general_test.length, equals(2));
+  });
+}
