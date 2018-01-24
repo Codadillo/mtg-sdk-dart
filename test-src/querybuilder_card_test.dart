d2bba22e22c5c8d643c8b3022a8dc33b242f083d
Codadillo <leoconr@nuevaschool.org>
2018-01-24 07:46:30 -0800

    Fixed some variable names.

--- test-src/querybuilder_card_test.dart
+++ test-src/querybuilder_card_test.dart
@@ -3,31 +3,31 @@ import '../lib/mtg_sdk_dart.dart';
 
 void main() {
   test("card.find() should return the card of the given id.", () async {
-    dynamic teferis_imp = await card.find(3369);
-    expect(teferis_imp["name"], equals("Teferi's Imp"));
-    expect(teferis_imp["manaCost"], equals("{2}{U}"));
-    expect(teferis_imp["cmc"], equals(3));
-    expect(teferis_imp["colors"], equals(["Blue"]));
-    expect(teferis_imp["type"], equals("Creature — Imp"));
-    expect(teferis_imp["rarity"], equals("Rare"));
-    expect(teferis_imp["set"], equals("MIR"));
-    expect(teferis_imp["text"], contains("Whenever Teferi's Imp phases in, draw a card."));
-    expect(teferis_imp["artist"], equals("Una Fricker"));
-    expect(teferis_imp["flavor"], equals("Made you look."));
+    dynamic card_find = await card.find(3369);
+    expect(card_find["name"], equals("Teferi's Imp"));
+    expect(card_find["manaCost"], equals("{2}{U}"));
+    expect(card_find["cmc"], equals(3));
+    expect(card_find["colors"], equals(["Blue"]));
+    expect(card_find["type"], equals("Creature — Imp"));
+    expect(card_find["rarity"], equals("Rare"));
+    expect(card_find["set"], equals("MIR"));
+    expect(card_find["text"], contains("Whenever Teferi's Imp phases in, draw a card."));
+    expect(card_find["artist"], equals("Una Fricker"));
+    expect(card_find["flavor"], equals("Made you look."));
   });
 
   test("card.where() should return the cards of the given properties.", () async {
-    dynamic where_general_test = await card.where({"set": "MIR", "colors": ["blue", "black"]});
-    where_general_test.forEach((c) => expect(c["colors"], equals(["Blue", "Black"])));
-    where_general_test.forEach((c) => expect(c["set"], equals("MIR")));
-    expect(where_general_test.length, equals(5));
+    dynamic where_general = await card.where({"set": "MIR", "colors": ["blue", "black"]});
+    where_general.forEach((c) => expect(c["colors"], equals(["Blue", "Black"])));
+    where_general.forEach((c) => expect(c["set"], equals("MIR")));
+    expect(where_general.length, equals(5));
   });
 
   test("card.where() should be able to control the page number and size", () async {
-    dynamic where_page_test = await card.where({"set": "MIR", "page": 2, "pageSize": 54});
-    expect(where_page_test.first["name"], equals("Forbidden Crypt"));
-    expect(where_page_test.last["name"], equals("Merfolk Raiders"));
-    expect(where_page_test.length, equals(54));
-    expect(where_page_test.runtimeType.toString(), equals("List"));
+    dynamic where_page = await card.where({"set": "MIR", "page": 2, "pageSize": 54});
+    expect(where_page.first["name"], equals("Forbidden Crypt"));
+    expect(where_page.last["name"], equals("Merfolk Raiders"));
+    expect(where_page.length, equals(54));
+    expect(where_page.runtimeType.toString(), equals("List"));
   });
 }
--- test-src/querybuilder_set_test.dart
+++ test-src/querybuilder_set_test.dart
@@ -3,22 +3,22 @@ import '../lib/mtg_sdk_dart.dart';
 
 void main() {
   test("set.find() should return the set of the given id.", () async {
-    dynamic mirage = await set.find("MIR");
-    expect(mirage["name"], equals("Mirage"));
-    expect(mirage["border"], equals("black"));
+    dynamic set_find = await set.find("MIR");
+    expect(set_find["name"], equals("Mirage"));
+    expect(set_find["border"], equals("black"));
   });
 
   test("set.where() should return sets of the given properties.", () async {
-    dynamic where_general_test = await set.where({"type": "un"});
-    expect(where_general_test.first["name"], equals("Unstable"));
-    expect(where_general_test.first["border"], equals("silver"));
-    expect(where_general_test.length, equals(3));
+    dynamic where_general = await set.where({"type": "un"});
+    expect(where_general.first["name"], equals("Unstable"));
+    expect(where_general.first["border"], equals("silver"));
+    expect(where_general.length, equals(3));
   });
 
   test("set.where() should be able to control the page number and size", () async {
-    dynamic where_general_test = await set.where({"page": "4", "pageSize": "2"});
-    expect(where_general_test.first["name"], equals("Super Series"));
-    expect(where_general_test.first["border"], equals("black"));
-    expect(where_general_test.length, equals(2));
+    dynamic where_page = await set.where({"page": "4", "pageSize": "2"});
+    expect(where_page.first["name"], equals("Super Series"));
+    expect(where_page.first["border"], equals("black"));
+    expect(where_page.length, equals(2));
   });
 }
