4050370911e6f00815db747c206416009bf134dc
Codadillo <leoconr@nuevaschool.org>
2018-01-24 18:42:21 -0800

    Added functions to limit number of pages or results returned.

--- lib/src/querybuilder.dart
+++ lib/src/querybuilder.dart
@@ -1,23 +1,25 @@
 import 'package:http/http.dart';
 import 'dart:convert';
 import 'dart:async';
-import 'custom_exceptions.dart';
+import 'query_exceptions.dart';
 
 final Client _client = new Client();
 const String apiUrl = "http://api.magicthegathering.io/v1";
 final Card cards = new Card();
 final Set sets = new Set();
 
-abstract class Product {
+abstract class QueryBuilder {
   String get endpoint;
+  int resultCap = 10000000;
+  int pageCap = 100000000;
 
   Future<dynamic> findProduct(dynamic id) async {
     final String url = "$apiUrl/$endpoint/$id";
     final Response response = await _client.get(url);
     final dynamic decodedResponse = JSON.decode(response.body);
     if (decodedResponse.containsKey("error"))
-      throw new QueryException(
-              decodedResponse["status"], decodedResponse["error"], url, "id: $id")
+      throw new QueryException(decodedResponse["status"],
+              decodedResponse["error"], url, "id: $id")
           .message;
     return decodedResponse.values.first;
   }
@@ -30,16 +32,18 @@ abstract class Product {
               decodedResponse["error"], url, properties)
           .message;
     decodedResponse = decodedResponse.values.first;
+    if (decodedResponse.isEmpty)
+      throw new QueryException("404", "Not Found", url, properties).message;
     if (properties.containsKey("page")) return decodedResponse;
     int page = 1;
     dynamic pageResponse = [-1];
-    while (pageResponse.isNotEmpty) {
+    properties["pageSize"] ??= 100;
+    while (!pageResponse.isEmpty && properties["pageSize"] * page <= resultCap && page <= pageCap) {
       ++page;
       pageResponse =
           JSON.decode((await _client.get("$url&page=$page")).body).values.first;
       decodedResponse.addAll(pageResponse);
     }
-    if (decodedResponse.values.first.isEmpty()) throw new QueryException("404", "Not Found", url, properties);
     return decodedResponse;
   }
 
@@ -57,9 +61,19 @@ abstract class Product {
     }
     return a;
   }
+
+  QueryBuilder limitResults(int resultCap) {
+    this.resultCap = resultCap;
+    return this;
+  }
+
+  QueryBuilder limitPages(int pageCap) {
+    this.pageCap = pageCap;
+    return this;
+  }
 }
 
-class Card extends Product {
+class Card extends QueryBuilder {
   String get endpoint => 'cards';
 
   Future<dynamic> find(final int id) async {
@@ -67,10 +81,15 @@ class Card extends Product {
   }
 }
 
-class Set extends Product {
+class Set extends QueryBuilder {
   String get endpoint => 'sets';
 
   Future<dynamic> find(final String id) async {
     return await super.findProduct(id);
   }
+}
+
+main() async {
+  print(await cards.limitResults(100).where({"name": "Storm Crow"}));
+
 }
